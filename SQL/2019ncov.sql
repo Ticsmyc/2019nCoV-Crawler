e2503de7de19ffaf29d17f74ee48ce1b70bdd742
Ticsmyc <ticsmyc@outlook.com>
2020-02-03 10:04:50 +0800

    更新表结构

--- SQL/2019ncov.sql
+++ SQL/2019ncov.sql
@@ -1,17 +1,17 @@
 /*
  Navicat Premium Data Transfer
 
- Source Server         : 本地mysql
+ Source Server         : 本地tomcat
  Source Server Type    : MySQL
- Source Server Version : 80019
+ Source Server Version : 50729
  Source Host           : localhost:3306
  Source Schema         : 2019ncov
 
  Target Server Type    : MySQL
- Target Server Version : 80019
+ Target Server Version : 50729
  File Encoding         : 65001
 
- Date: 28/01/2020 01:10:44
+ Date: 03/02/2020 10:04:18
 */
 
 SET NAMES utf8mb4;
@@ -28,7 +28,7 @@ CREATE TABLE `city`  (
   `curedCount` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `deadCount` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `provinceName` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `modifyTime` int(0) NULL DEFAULT NULL
+  `modifyTime` int(11) NULL DEFAULT NULL
 ) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
@@ -43,7 +43,7 @@ CREATE TABLE `province`  (
   `curedCount` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `deadCount` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `comment` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `modifyTime` int(0) NULL DEFAULT NULL
+  `modifyTime` int(11) NULL DEFAULT NULL
 ) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
@@ -51,9 +51,9 @@ CREATE TABLE `province`  (
 -- ----------------------------
 DROP TABLE IF EXISTS `statistics`;
 CREATE TABLE `statistics`  (
-  `id` int(0) NULL DEFAULT NULL,
-  `createTime` int(0) NULL DEFAULT NULL,
-  `modifyTime` int(0) NULL DEFAULT NULL,
+  `id` int(11) NULL DEFAULT NULL,
+  `createTime` int(11) NULL DEFAULT NULL,
+  `modifyTime` int(11) NULL DEFAULT NULL,
   `infectSource` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `passWay` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `imgUrl` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
@@ -68,10 +68,11 @@ CREATE TABLE `statistics`  (
   `remark4` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `remark5` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `generalRemark` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `confirmedCount` int(0) NULL DEFAULT NULL,
-  `suspectedCount` int(0) NULL DEFAULT NULL,
-  `curedCount` int(0) NULL DEFAULT NULL,
-  `deadCount` int(0) NULL DEFAULT NULL
+  `confirmedCount` int(11) NULL DEFAULT NULL,
+  `suspectedCount` int(11) NULL DEFAULT NULL,
+  `curedCount` int(11) NULL DEFAULT NULL,
+  `deadCount` int(11) NULL DEFAULT NULL,
+  `seriousCount` int(11) NULL DEFAULT NULL
 ) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
@@ -79,17 +80,17 @@ CREATE TABLE `statistics`  (
 -- ----------------------------
 DROP TABLE IF EXISTS `timeline`;
 CREATE TABLE `timeline`  (
-  `id` int(0) NOT NULL,
-  `pubDate` int(0) NULL DEFAULT NULL,
+  `id` int(11) NOT NULL,
+  `pubDate` int(11) NULL DEFAULT NULL,
   `pubDateStr` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `title` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `summary` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `infoSource` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `sourceUrl` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `provinceId` int(0) NULL DEFAULT NULL,
+  `provinceId` int(11) NULL DEFAULT NULL,
   `provinceName` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `createTime` int(0) NULL DEFAULT NULL,
-  `modifyTime` int(0) NULL DEFAULT NULL,
+  `createTime` int(11) NULL DEFAULT NULL,
+  `modifyTime` int(11) NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
